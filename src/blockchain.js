eaa0e8ff12f9e5fd809fec0a68ecf36130777ca4
GeonLee <leegun2003@gmail.com>
2019-05-26 18:54:32 +0900

    feature: genesis block study: genesis block

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,15 @@ class Block {
     this.data = data;
   }
 }
+
+const genesisBlock = new Block(
+  0,
+  "1d0314bbcd2c6ed7dc73a070b048493d9061614a5a8109d6d438669f1bc77392",
+  null,
+  1558864199886,
+  "This is the 🧔genesis!!"
+);
+
+let Blockchain = genesisBlock;
+
+console.log(Blockchain);
