39d148d39b9eb001abe64bd1edc3184a2b073088
GeonLee <leegun2003@gmail.com>
2019-05-27 23:36:31 +0900

    chore: add ws

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import * as morgan from 'morgan';
-import * as dotenv from 'dotenv';
-import { getBlockchain, createNewBlock } from './blockchain';
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import * as morgan from "morgan";
+import * as dotenv from "dotenv";
+import { getBlockchain, createNewBlock } from "./blockchain";
 
 dotenv.config();
 
-const PORT: string = process.env.PORT;
+const PORT: number = parseInt(process.env.HTTP_PORT) || 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
-app.get('/blocks', (req: express.Request, res: express.Response) => {
+app.get("/blocks", (req: express.Request, res: express.Response) => {
   res.send(getBlockchain());
 });
 
-app.post('/blocks', (req: express.Request, res: express.Response) => {
+app.post("/blocks", (req: express.Request, res: express.Response) => {
   const {
     body: { data }
   } = req;
